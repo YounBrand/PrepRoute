1084b786d78403839521fc6a55ff1a49fbcdc45b
3clipse <mdam0802work@gmail.com>
2025-09-09 16:40:21 -0400

    Migrated to ES6 modules. If we're using a frontend framework, path will not be needed

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const path = require('path');
+import express from "express";
+import path from "path";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
 module.exports = app;
 
--- test/server.test.js
+++ test/server.test.js
@@ -1,10 +1,10 @@
-const request = require('supertest');
-const app = require('../src/server.js');
+import request from "supertest";
+import app from "../src/server.js";
 
-describe('GET /', () => {
-  it('should return the index.html file', async () => {
-    const response = await request(app).get('/');
+describe("GET /", () => {
+  it("should return the index.html file", async () => {
+    const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
-    expect(response.headers['content-type']).toContain('text/html');
+    expect(response.headers["content-type"]).toContain("text/html");
   });
 });
